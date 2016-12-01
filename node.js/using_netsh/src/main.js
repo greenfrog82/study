1176b10c4b376509fa4c6a8beb8e57bbe43dc801
DESKTOP-869DHIP\greenfrog <greenfrog82@naver.com>
2016-12-01 10:45:19 +0900

    방화벽 뚫는 코드 원본

--- node.js/using_netsh/src/main.js
+++ node.js/using_netsh/src/main.js
@@ -1,4 +1,5 @@
 const process = require('child_process').exec;
+// const spawn = require('child_process').spawn;
 const os = require('os');
 
 console.log('OS : ', os.platform());
@@ -14,29 +15,28 @@ function OpenFirewallForWindows(serverName, port) {
     process(`netstat -na | findstr /r /c:\".*TCP.*:${port} \"`, (err, stdout, stderr) => {
       // 위 내용으로 열려있는 포트를 못찾으면 에러 발생!
       if(err) {
-        console.error(`exec error: ${err}`);
+        console.log(`1. exec error: ${err}`);
         return resolve();
       }
 
       // 이미 존재하는 포트가 있다는 응답.
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
+      console.log(`1. stdout: ${stdout}`);
+      console.log(`1. stderr: ${stderr}`);
 
       reject('Found');
     });
   })
   .then(
-    () =>{
-      console.log('Not Found');
+    () => {
       const cmd = `netsh advfirewall firewall add rule name=\"${serverName}:${port}\" dir=in action=allow protocol=TCP localport=${port}`;
       process(cmd, (err, stdout, stderr) => {
         if(err) {
-          console.error(`exec error: ${err}`);
+          console.error(`2. exec error: ${err}`);
           return;
         }
         // 이미 존재하는 포트가 있다는 응답.
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
+        console.log(`2. stdout: ${stdout}`);
+        // console.log(`2. stderr: ${stderr}`);
       });
     },
     (reseaon)=> {
@@ -45,15 +45,18 @@ function OpenFirewallForWindows(serverName, port) {
   );
 }
 
-OpenFirewallForWindows(serverName, port)
-// netstat.stdout.on('data', data => {
-//   console.log(`stdout: ${data}`);
-// });
+OpenFirewallForWindows(serverName, port);
+
 //
-// netstat.stderr.on('data', data => {
-//   console.log(`stderr: ${data}`);
-// });
-//
-// netstat.on('close', code => {
-//   console.log(`child process exited with code ${code}`);
-// });
+// OpenFirewallForWindows(serverName, port);
+// // netstat.stdout.on('data', data => {
+// //   console.log(`stdout: ${data}`);
+// // });
+// //
+// // netstat.stderr.on('data', data => {
+// //   console.log(`stderr: ${data}`);
+// // });
+// //
+// // netstat.on('close', code => {
+// //   console.log(`child process exited with code ${code}`);
+// // });
