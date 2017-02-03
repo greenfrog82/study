88001281032dc938e36b4752c9b292bb90f5db38
greenfrog <greenfrog82@naver.com>
2017-02-03 10:40:27 +0900

    파이썬 패키지 시스템 공부 중 발생한 오류에 대해서 연구 중 ..
    ValueError: Attempted relative import beyond toplevel package

--- python/Basic/module/src/game/import_2.py
+++ python/Basic/module/src/game/import_2.py
@@ -1,4 +1,4 @@
-from graphic.render import test_render, test_render2
+from src.game.graphic.render import test_render, test_render2
 
 test_render()
 test_render2()
--- python/Basic/module/src/game/import_3.py
+++ python/Basic/module/src/game/import_3.py
@@ -1,3 +1,13 @@
-import graphic.render
+import src.game.graphic.render
 
-graphic.render.test_render()
+# This code work well.
+# src.game.graphic.render.test_render()
+
+# This code occurs the following error. Because import operator just mark that src.game.graphic.render is able to use in this module.
+#
+# /usr/bin/python3.5 /home/jaeyoungcho/develop/github/study/python/Basic/module/src/game/import_3.py
+# Traceback (most recent call last):
+#  File "/home/jaeyoungcho/develop/github/study/python/Basic/module/src/game/import_3.py", line 4, in <module>
+#    render.test_render()
+# NameError: name 'render' is not defined
+render.test_render()
