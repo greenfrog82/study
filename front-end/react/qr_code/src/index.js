ad8768604f79250c50164caf17108de14d54194b
jaeyoung.cho <greenfrog82@naver.com>
2017-08-04 13:52:55 +0900

    webpack.config.js has been changed by officer guide.

--- front-end/react/qr_code/src/index.js
+++ front-end/react/qr_code/src/index.js
@@ -5,4 +5,7 @@ import QRCode from 'qrcode.react';
 
 const rootElement = document.getElementById('root');
 // ReactDOM.render(<App />, rootElement);
-ReactDOM.render(<QRCode value="otpauth://totp/qr%20test:aaa%40cdnetworks.com?secret=base32secret3232&issuer=qr%20test" />, rootElement);
+//ReactDOM.render(<QRCode value="otpauth://totp/qr%20test:aaa%40cdnetworks.com?secret=base32secret3232&issuer=qr%20test" />, rootElement);
+// ReactDOM.render(<QRCode value="otpauth://totp/jaeyoung:jaeyoung.cho%40cdnetworks.com?secret=base32secret3232&issuer=jaeyoung" />, rootElement);
+// ReactDOM.render(<QRCode value="otpauth://totp/aurora:jaeyoung.cho%40cdnetworks.com?secret=base32secret3232&issuer=aurora" />, rootElement);
+ReactDOM.render(<QRCode value="otpauth://totp/aurora:1%40cdnetworks.com?secret=JBSWY3DPEHPK3PXP&issuer=aurora" />, rootElement);
--- front-end/react/qr_code/webpack.config.js
+++ front-end/react/qr_code/webpack.config.js
@@ -1,28 +1,27 @@
+const path = require('path');
+
 module.exports = {
-    entry: './src/index.js',
-
-    output: {
-        path: __dirname + '/public/',
-        filename: 'bundle.js'
-    },
-
-    devServer: {
-        inline: true,
-        port: 7777,
-        contentBase: __dirname + '/public/'
-    },
-
-    module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    loader: 'babel-loader',
-                    exclude: /node_modules/,
-                    query: {
-                        cacheDirectory: true,
-                        presets: ['es2015', 'react']
-                    }
-                }
-            ]
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    inline: true,
+    port: 7777,
+    contentBase: __dirname + '/dist/'
+  },
+  module: {
+    loaders: [
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                cacheDirectory: true,
+                presets: ['es2015', 'react']
+            }
         }
+    ]
+  }
 };
