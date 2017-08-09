00fa1d00ab46718b8283a6050668eec00cb51d21
jaeyoung.cho <greenfrog82@naver.com>
2017-08-09 17:28:49 +0900

    Quick start is done. And to generate qr code example is done.

---
+++ front-end/vue/qr_code/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <qrcode :val="val" :size="size" :bg-color="bgColor" :fg-color="fgColor" level="L"></qrcode> -->
+    <!-- <qrcode :val='val', :size='size'></qrcode> -->
+    <qrcode-vue :value='value' :size='size' level='H'></qrcode-vue>
+  </div>
+</template>
+
+<script>
+// import Qrcode from 'vue-qrcode';
+import QrcodeVue from 'qrcode.vue';
+//
+export default {
+  components: {
+    QrcodeVue
+  },
+  data() {
+    return {
+      value: 'otpauth://totp/aurora:1%40cdnetworks.com?secret=JBSWY3DPEHPK3PXP&issuer=aurora',
+      size: 300
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front-end/vue/quick_start/readme.md
@@ -0,0 +1,6 @@
+# Quick Start 
+
+
+## 참조
+
+* [Vue.js 2 Quickstart Tutorial 2017](https://medium.com/codingthesmartway-com-blog/vue-js-2-quickstart-tutorial-2017-246195cfbdd2)
