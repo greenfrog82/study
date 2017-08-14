8a4d5acb270189ef96539ee6bb532f4800f665a6
jaeyoung.cho <greenfrog82@naver.com>
2017-08-14 15:19:09 +0900

    Modal dialog example in vue has been changed to apply single file component.

--- front-end/vue/modal_dialog/src/App.vue
+++ front-end/vue/modal_dialog/src/App.vue
@@ -1,103 +1,36 @@
 <template>
-  <!-- app -->
   <div id="app">
     <button id="show-modal" @click="showModal = true">Show Modal</button>
     <!-- use the modal component, pass in the prop -->
-    <modal v-if="showModal" @close="showModal = false">
+    <!-- <modal v-if="showModal" @close="showModal = false"> -->
       <!--
         you can use custom content here to overwrite
         default content
       -->
+      <!-- <h3 slot="header">custom header</h3> -->
+    <!-- </modal> -->
+    <modal-template v-if="showModal" @close='showModal=false'>
       <h3 slot="header">custom header</h3>
-    </modal>
+    </modal-template>
   </div>
 </template>
 
-<script type="text/x-template" id="modal-template">
-  <transition name="modal">
-    <div class="modal-mask">
-      <div class="modal-wrapper">
-        <div class="modal-container">
+<script>
+import ModalTemplate from './components/Modal.vue'
 
-          <div class="modal-header">
-            <slot name="header">
-              default header
-            </slot>
-          </div>
-
-          <div class="modal-body">
-            <slot name="body">
-              default body
-            </slot>
-          </div>
-
-          <div class="modal-footer">
-            <slot name="footer">
-              default footer
-              <button class="modal-default-button" @click="$emit('close')">
-                OK
-              </button>
-            </slot>
-          </div>
-        </div>
-      </div>
-    </div>
-  </transition>
+export default {
+  components: {
+    modalTemplate: ModalTemplate
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  }
+}
 </script>
 
+
 <style>
-
-
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
-}
-
-.modal-container {
-  width: 300px;
-  margin: 0px auto;
-  padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-  transition: all .3s ease;
-  font-family: Helvetica, Arial, sans-serif;
-}
-
-.modal-header h3 {
-  margin-top: 0;
-  color: #42b983;
-}
-
-.modal-body {
-  margin: 20px 0;
-}
-
-.modal-default-button {
-  float: right;
-}
-
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
-
-.modal-enter {
-  opacity: 0;
-}
-
-.modal-leave-active {
-  opacity: 0;
-}
-
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
 
 </style>
--- front-end/vue/modal_dialog/src/main.js
+++ front-end/vue/modal_dialog/src/main.js
@@ -1,19 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import App from './App'
+import App from './App'
 
 Vue.config.productionTip = false
 
-Vue.component('modal', {
-  template: '#modal-template'
-});
 
 new Vue({
   el: '#app',
-  data: {
-    showModal: false
-  }
+  template: '<App />',
+  components: { App }
 })
 
 // new Vue({
