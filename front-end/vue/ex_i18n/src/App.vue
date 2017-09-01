472f2f231047cf6be4fcf98b856eb0ecd6b59641
greenfrog <greenfrog82@naver.com>
2017-09-01 14:34:53 +0900

    [front-end/vue] About i18n with vue.js

---
+++ front-end/vue/ex_i18n/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <label for="locale">locale</label>
+    <select v-model="locale">
+      <option>en</option>
+      <option>jp</option>
+    </select>
+    <p>message: {{ $t('message.hello') }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      locale: 'en'
+    }
+  },
+  watch: {
+    locale(val) {
+      this.$i18n.locale = val;
+    }
+  },
+  created() {
+    // console.log(this.$i18n.locale);
+    this.$i18n.locale = 'jp';
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
