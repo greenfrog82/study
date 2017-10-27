00642d0d895b1b10e5825b89d237d2f6529441dc
greenfrog <greenfrog82@naver.com>
2017-10-27 10:37:56 +0900

    [Algorithm] Finding substring has been modified.

--- algorithm/etc/find_substring/test_resolve.py
+++ algorithm/etc/find_substring/test_resolve.py
@@ -1,9 +1,9 @@
 import unittest2 as unittest
 from resolve import perform
 
-class TestMproxyAdmin(unittest.TestCase):
+class TestAlgorithm(unittest.TestCase):
     def test_case_1(self):
-        self.assertEqual(perform('101011'), 3)
+        self.assertEqual(perform('101011'), 4)
 
     def test_case_2(self):
         self.assertEqual(perform('11001'), 3)
@@ -13,9 +13,18 @@ class TestMproxyAdmin(unittest.TestCase):
 
     def test_case_4(self):
         self.assertEqual(perform('11110'), 1)        
+
+    def test_case_5(self):
+        self.assertEqual(perform('10000'), 1)        
+
+    def test_case_6(self):
+        self.assertEqual(perform('00000'), 0)        
+
+    def test_case_7(self):
+        self.assertEqual(perform('11111'), 0)        
+
+    def test_case_7(self):
+        self.assertEqual(perform('01110'), 2)        
         
-
-
-
 if __name__ == "__main__":
     unittest.main()
