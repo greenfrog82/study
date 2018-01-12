e042b4cbc797131c55aa737959e546c25ce30b08
greenfrog <jaeyoung.cho@cddnetworks.co.kr>
2018-01-12 18:15:30 +0900

    How to deal with big data with jQuery and vue.js.

---
+++ front-end/jQuery/uncaught_rangeerror_max_call_stack_size_exceeded/vue-test/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <button v-on:click=this.clear>Clear</button>
+    <select>
+      <option v-for="item in generatedItems" :value="item">{{ item }}</option>
+    </select>
+    <!-- <select>
+      <optison
+    </select>     -->
+
+    
+    
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'app',
+  data() {
+    return {
+      items: []
+    }
+  },
+  methods: {
+    clear() {      
+      console.log('Click clear button');
+      this.items = []
+    }
+  },
+  computed: {
+    generatedItems() {
+      console.log('generatedItem');
+      for(var i=0; i<80000; i++) {
+        this.items.push(i);
+      }
+      return this.items;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
