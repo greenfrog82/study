071b65d100a77e00579d39634dd8ee003e784cc6
greenfrog <greenfrog82@naver.com>
2018-01-13 14:17:12 +0900

    How to start a Django project

--- python/Django/how_to_start_django_project/src/study_django/settings.py
+++ python/Django/how_to_start_django_project/src/study_django/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'study_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'django_sdy',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'docker.for.mac.localhost',
+        'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
