fcf5d1ab99fd6a80a2c2691eaddbf72a133069bb
greenfrog <greenfrog82@naver.com>
2018-01-19 01:50:11 +0900

    About in operator in python

---
+++ python/Basic/in/src/ex.py
@@ -0,0 +1,19 @@
+values = [1, 2, 3]
+
+print '3 in values : ', 3 in values
+print '4 in values : ', 4 in values
+print '3 not in values : ', 3 not in values
+print '4 not in values : ', 4 not in values
+
+values = {
+    'name': 'greenfrog',
+    'job': 'programmer',
+}
+
+print 'name in values : ', 'name' in values
+print 'hobby in values : ', 'hobby' in values
+
+str = 'greenfrog'
+
+print 'g in str : ', 'g' in str
+print 'a in str : ', 'a' in str
