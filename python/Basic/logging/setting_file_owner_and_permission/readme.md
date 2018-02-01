b85f3712b1641795ded6144b781f87209d4e4f87
greenfrog <greenfrog82@naver.com>
2018-02-02 01:44:24 +0900

    How to set file owner and permission and apply these setting to created file.

---
+++ python/Basic/logging/setting_file_owner_and_permission/readme.md
@@ -0,0 +1,15 @@
+# How to set file owner and permission and apply these setting to created file.
+
+
+## Reference
+
+* [Customizing handlers with dictConfig()
+](https://docs.python.org/2/howto/logging-cookbook.html#customizing-handlers-with-dictconfig)
+* [Does python logging.handlers.RotatingFileHandler allow creation of a group writable log file?](https://stackoverflow.com/questions/1407474/does-python-logging-handlers-rotatingfilehandler-allow-creation-of-a-group-writa)
+* [stat — Interpreting stat() results](https://docs.python.org/2/library/stat.html#module-stat)
+* [How to change the user and group permissions for a directory, by name?
+](https://stackoverflow.com/questions/5994840/how-to-change-the-user-and-group-permissions-for-a-directory-by-namej)
+* [How can I get a file's permission mask?
+](https://stackoverflow.com/questions/5337070/how-can-i-get-a-files-permission-mask)
+* [how to find the owner of a file or directory in python
+](https://stackoverflow.com/questions/1830618/how-to-find-the-owner-of-a-file-or-directory-in-python)
---
+++ python/Basic/logging/setting_file_owner_and_permission/src/main.py
@@ -0,0 +1,92 @@
+import logging
+import logging.config
+import pwd
+import grp
+import os
+import stat
+
+class GroupWriteRotatingFileHandler(logging.handlers.RotatingFileHandler):
+    def doRollover(self):
+        """
+        Override base class method to make the new log file group writable.
+        """
+        # Rotate the file first.
+        logging.handlers.RotatingFileHandler.doRollover(self)
+
+        # Set file permission
+        rotated_file_name = '%s.1' % self.baseFilename
+        currMode = os.stat(rotated_file_name).st_mode
+        os.chmod(self.baseFilename, currMode)
+
+        # Set file owner
+        uid = os.stat(rotated_file_name).st_uid
+        gid = os.stat(rotated_file_name).st_gid 
+        os.chown(self.baseFilename, uid, gid)
+        
+        #os.chmod(self.baseFilename, currMode)
+
+def owned_file_handler(filename, mode='a', encoding=None, owner=None, maxBytes=0, backupCount=0, chmod=None):
+    import pdb;pdb.set_trace()
+    uid = pwd.getpwnam(owner[0]).pw_uid
+    gid = grp.getgrnam(owner[1]).gr_gid
+
+    if owner:
+        if not os.path.exists(filename):
+            open(filename, 'a').close()
+        os.chown(filename, uid, gid)
+        os.chmod(filename, chmod)
+    #logging.handlers.RotatingFileHandler(
+    #    filename, mode='a', maxBytes=0, backupCount=0, encoding=None, delay=0)
+    #return logging.FileHandler(filename, mode, encoding)
+    #return logging.handlers.RotatingFileHandler(filename, mode='a', maxBytes=10, backupCount=10)
+    return GroupWriteRotatingFileHandler(filename, maxBytes=maxBytes, backupCount=backupCount)
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
+        },
+    },
+    'handlers': {
+        # 'file': {
+        #     # The values below are popped from this dictionary and
+        #     # used to create the handler, set the handler's level and
+        #     # its formatter.
+        #     '()': owned_file_handler,
+        #     'level': 'DEBUG',
+        #     'formatter': 'default',
+        #     # The values below are passed to the handler creator callable
+        #     # as keyword arguments.
+        #     'owner': ['www-data', 'www-data'],
+        #     'filename': 'chowntest.log',
+        #     'mode': 'w',
+        #     'encoding': 'utf-8',
+        # },
+        'file2': {
+            '()': owned_file_handler,
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'filename': 'test.log',
+            'owner': ['www-data', 'www-data'],
+            'chmod': 0660,
+            'maxBytes': 60,
+            'backupCount': 10
+        }
+    },
+    'root': {
+        'handlers': ['file2'],
+        'level': 'DEBUG',
+    },
+}
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger('mylogger')
+
+for idx in range(0, 10):
+    logger.debug('%d > A debug message' % idx)
+
+
+#print oct(os.stat("test.log").st_mode & 0777)
