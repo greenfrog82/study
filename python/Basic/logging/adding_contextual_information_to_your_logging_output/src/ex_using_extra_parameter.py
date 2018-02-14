d3f5f2a8355dba50fee05b14489160818504aa4d
greenfrog <greenfrog82@naver.com>
2018-02-14 22:49:51 +0900

    Adding contextual information to your logging output - using extra parameter.

---
+++ python/Basic/logging/adding_contextual_information_to_your_logging_output/src/ex_using_extra_parameter.py
@@ -0,0 +1,33 @@
+import logging
+import logging.config
+import os
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(idx)d %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'default',
+            'filename': 'test.log',
+            'maxBytes': 1024,
+            'backupCount': 10,
+        }
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'DEBUG',
+    },
+}
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
+
+for idx in range(10):
+    logger.debug('message', extra={'idx': idx})
