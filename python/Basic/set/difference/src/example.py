3194c8f842206f024864ec9048776c357364d250
greenfrog <greenfrog82@naver.com>
2018-02-26 22:37:52 +0900

    About difference method of Set

---
+++ python/Basic/set/difference/src/example.py
@@ -0,0 +1,31 @@
+# ---------------------------------------------------
+
+A = {'a', 'b', 'c', 'd'}
+B = {'c', 'f', 'g'}
+
+# Equivalent to A-B
+print 'A.difference(B) : ', A.difference(B)
+
+# Equivalent to B-A
+print 'B.difference(A) : ', B.difference(A)
+
+
+# ---------------------------------------------------
+
+A = {'a', 'b', 'c'}
+B = {'d', 'e', 'f'}
+
+print 'A.difference(B) : ', A.difference(B)
+print 'B.difference(A) : ', B.difference(A)
+
+# ---------------------------------------------------
+
+A = {'a', 'b', 'c', 'd'}
+B = {'c', 'f', 'g'}
+
+print 'A.difference(B) : ', A.difference(B)
+print 'B.difference(A) : ', B.difference(A)
+
+print 'A : ', A
+print 'B : ', B
+
