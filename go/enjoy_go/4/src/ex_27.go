afcb45d58e433a3751d6b24c5d09f39a2d04586f
jaeyoung.cho <jaeyoungcho-n@jaeyoungchos-MacBook-Pro.local>
2018-03-30 15:33:29 +0900

    4 week study Golang

--- go/enjoy_go/4/src/ex_27.go
+++ go/enjoy_go/4/src/ex_27.go
@@ -50,10 +50,10 @@ func main() {
 	// Recover is a built-in function that regains control of a panicking goroutine.
 	// Recover is only useful inside deferred functions. During normal execution, a call to recover will return nil and have no other effect. 
 	// If the current goroutine is panicking, a call to recover will capture the value given to panic and resume normal execution.
-	defer func() {
-		s := recover()
-		fmt.Println(s)
-	}()
+	//defer func() {
+	//	s := recover()
+	//	fmt.Println(s)
+	//}()
 
 	value := 0
 	res := 100 / value
