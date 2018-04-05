2ee761635749848d38db68869a8c0d236f5aa1aa
greenfrog <greenfrog82@naver.com>
2018-04-06 00:44:37 +0900

    4kyu strip url params

--- algorithm/Codewars/4kyu_strip_url_params/src/main.js
+++ algorithm/Codewars/4kyu_strip_url_params/src/main.js
@@ -3,13 +3,22 @@ function stripUrlParams(url, paramsToStrip) {
     if(1 >= splitedUrl.length) {
         return url;
     } else {
-        // const matched = /(\w+)\=/g.exec(splitedUrl[1]);
-        const reg = new RegExp(/(\w+)\=/, 'g');
-        const matched = reg.exec(splitedUrl[1]);
-        console.log(matched);
+        const set = new Set();
+        let queryParam = '';
+        const params = splitedUrl[1].split('&');
+        params.forEach(param => {
+            const varName = param.split('=')[0];
+            if(set.has(varName) || ((paramsToStrip)? paramsToStrip.includes(varName): false)) {
+                return;
+            }
+            set.add(varName);
+            queryParam += ('' === queryParam)? param: `&${param}`;
+        });
+        return (queryParam)? `${splitedUrl[0]}?${queryParam}`: splitedUrl[0];
     }
 }
 
 console.log('www.codewars.com?a=1&b=2' == stripUrlParams('www.codewars.com?a=1&b=2&a=2'));
+console.log('www.codewars.com' == stripUrlParams('www.codewars.com?a=1&b=2&a=2', ['a','b']));
 console.log('www.codewars.com?a=1' == stripUrlParams('www.codewars.com?a=1&b=2&a=2', ['b']));
 console.log('www.codewars.com' == stripUrlParams('www.codewars.com', ['b']));
