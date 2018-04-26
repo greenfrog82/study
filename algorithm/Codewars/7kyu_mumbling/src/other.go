323f133540221676a6ee40b51c234bf16f19b2bb
greenfrog <greenfrog82@naver.com>
2018-04-27 08:29:48 +0900

    7kyu Mumbling

---
+++ algorithm/Codewars/7kyu_mumbling/src/other.go
@@ -0,0 +1,22 @@
+package other
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(Accum("abcd") == "A-Bb-Ccc-Dddd")
+	fmt.Println(Accum("RqaEzty") == "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy")
+	fmt.Println(Accum("cwAt") == "C-Ww-Aaa-Tttt")
+}
+
+func Accum(s string) string {
+	parts := make([]string, len(s))
+
+	for i:=0 i<len(s); i++ {
+		parts[i] = strings.ToUpper(string(s[i])) + stirngs.repeat(strings.ToLower(string(s[i]), i)
+	}
+
+	return strings.join(parts, "-")
+}
