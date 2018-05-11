aa1f156cd0a0a175e2d612d91101937a953850e6
greenfrog <greenfrog82@naver.com>
2018-05-11 12:12:07 +0900

    The features of TODO list has been complted.

--- front-end/vue/todo-list/src/src/store/index.js
+++ front-end/vue/todo-list/src/src/store/index.js
@@ -3,49 +3,57 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export class Todo {
-    constructor(content) {
-        this.state = true; // boolean, in case of ture, This todo is active. Or not this todo is completed.
-        this.content = content
-    }
+// export class Todo {
+//     constructor(key, content) {
+//         this.key = key
+//         this.done = false         
+//         this.content = content
+//     }
+// }
+
+function specificStateList(list, state) {
+    let ret = []
+    list.forEach(element => {
+        if(state == element.done) {
+            ret.push(element)
+        }
+    })
+
+    console.log(ret)
+    return ret
 }
 
 export default new Vuex.Store({
     state: {
-        todo: null,
-        todoList: []
+        todoList: [
+            {
+                key: 1,
+                done: false,
+                content: 'Go'
+            },
+            {
+                key: 2,
+                done: false,
+                content: 'Vue'
+            }
+        ]
     },
+    getters: {
+        activatedList(state) {
+            return specificStateList(state.todoList, false)
+        },
+        completedList(state) {
+            return specificStateList(state.todoList, true)
+        }
+    },
+    mutations: {
+        newTodo(state, { key, content } ) {
+            console.log(`${key} : ${content}`)
+            state.todoList.push({
+                key: key,
+                done: false,
+                content: content
+            });
+        },
+    }
 });
-
-
-
-state: {
-    username: null,
-        password: null
-},
-getters: {
-    combine(state) {
-        return `${state.username}:${state.password}`;
-    }
-},
-mutations: {
-    username(state, value) {
-        console.log('This is username mutation in store. value paramter is', value);
-        state.username = value;
-    },
-    password(state, value) {
-        console.log('This is password mutation in store. value paramter is', value);
-        state.password = value;
-    }
-},
-actions: {
-    change({ commit }) {
-        new Promise((resolve, reject) => {
-            commit('username', 'greenfrog');
-            commit('password', '123');
-            resolve();
-        }).then(() => {
-            console.log('finish');
-        });
-    }
-}
