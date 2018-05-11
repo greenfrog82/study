15d38c182b95269bc7680cb193c181f336ff3dd7
greenfrog <greenfrog82@naver.com>
2018-05-11 23:35:53 +0900

    3kyu Did you mean ...?

--- algorithm/Codewars/3kyu/3kyu_did_you_mean/src/main.py
+++ algorithm/Codewars/3kyu/3kyu_did_you_mean/src/main.py
@@ -5,64 +5,24 @@ class Dictionary(object):
     def find_most_similar(self, word):
         ret = {}
 
-        def get_modified_cnt(dict_word):
-            return len(dic_word) - len(same_ch_idx_list) if len(dic_word) > len(word) else len(word) - len(same_ch_idx_list)
-
         for dic_word in self.words:
             same_ch_idx_list = []
-            tmp = []
-            
             for i in range(len(dic_word)):
-                if not tmp and not same_ch_idx_list:
-                    start_idx = 0
-                elif tmp:
-                    start_idx = tmp[-1] + 1
-                else:
-                    start_idx = same_ch_idx_list[-1] + 1
-
+                start_idx = 0 if not same_ch_idx_list else same_ch_idx_list[-1] + 1
                 for j in range(start_idx, len(word)):
                     if dic_word[i] == word[j]:
-                        if i == j:
+                        if i == j or \
+                            (len(dic_word) > i+1 and len(word) > j+1 and dic_word[i+1] == word[j+1]) or \
+                            (same_ch_idx_list and same_ch_idx_list[-1] == j - 1):
                             same_ch_idx_list.append(j)
                             break
-                        elif not same_ch_idx_list or same_ch_idx_list[-1] == j - 1:
-                            same_ch_idx_list.append(j)
-                            break
-                        elif not tmp or tmp[-1] == j - 1:
-                            tmp.append(j)
-                            break
-                    elif 1 < len(tmp):
-                        same_ch_idx_list = same_ch_idx_list + tmp
-                        tmp = []
-                    elif 1 >= len(tmp):
-                        tmp = []
 
-            if 1 < len(tmp):
-                same_ch_idx_list = same_ch_idx_list + tmp
-                        
-            if not same_ch_idx_list:
-                continue
-
-            # print 'dic_word : %s / same_ch_idx_list : %s / len : %d' % (dic_word, str(same_ch_idx_list), len(same_ch_idx_list))
-
-            # if 'mars' == dic_word:
-            #     import pdb; pdb.set_trace()
-
-            if not ret:
+            modified_cnt = max(len(dic_word), len(word)) - len(same_ch_idx_list)
+            
+            if not ret or ret['modified_cnt'] > modified_cnt:
                 ret['word'] = dic_word
-                ret['modified_cnt'] = get_modified_cnt(dic_word)
+                ret['modified_cnt'] = modified_cnt
 
-                # print 'dic_word : %(word)s / modified_cnt : %(modified_cnt)d' % ret
-
-            else:
-                modified_cnt = get_modified_cnt(dic_word)
-                # print 'dic_word : %s / modified_cnt : %d' % (dic_word, modified_cnt)
-                
-                if ret['modified_cnt'] > modified_cnt:
-                    ret['word'] = dic_word
-                    ret['modified_cnt'] = modified_cnt
-
-        # print ret
         return ret['word']
                         
 fruits = Dictionary(['cherry', 'pineapple', 'melon', 'strawberry', 'raspberry'])
