3c75d9494bfafc3a47f51a366869da69220a2c33
greenfrog <greenfrog82@naver.com>
2018-05-24 12:05:12 +0900

    [DjangoRestFramework][Tutorial 4: Authentication and Permissions

--- python/djangorestframework/tutorial/snippets/models.py
+++ python/djangorestframework/tutorial/snippets/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from pygments import highlight
 from pygments.lexers import get_all_lexers
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
 from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -7,6 +10,8 @@ LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 class Snippet(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
@@ -17,3 +22,10 @@ class Snippet(models.Model):
     class Meta:
         ordering = ('created', )
 
+    def save(self, *args, **kwargs):
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
--- python/djangorestframework/tutorial/snippets/urls.py
+++ python/djangorestframework/tutorial/snippets/urls.py
@@ -5,6 +5,8 @@ from snippets import views
 urlpatterns = [
     re_path(r'^snippets/$', views.SnippetList.as_view()),
     re_path(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    re_path(r'^users/$', views.UserList.as_view()),
+    re_path(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
