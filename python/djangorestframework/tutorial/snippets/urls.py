67bcf796ece4f5abf169aac5c6ffefba49c0cdbd
greenfrog <greenfrog82@naver.com>
2018-05-24 09:54:30 +0900

    [DjangoRestFramework][Tutorial 2:Requests and Responses]

--- python/djangorestframework/tutorial/snippets/urls.py
+++ python/djangorestframework/tutorial/snippets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import re_path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     re_path(r'^snippets/$', views.snippet_list),
     re_path(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
