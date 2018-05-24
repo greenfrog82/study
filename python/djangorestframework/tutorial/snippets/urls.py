20d4f87297447884aaa87b2966536064fcdf4066
greenfrog <greenfrog82@naver.com>
2018-05-24 10:16:24 +0900

    [DjangoRestFramework][Tutorial 3: Class-based Views] Using class-based views

--- python/djangorestframework/tutorial/snippets/urls.py
+++ python/djangorestframework/tutorial/snippets/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    re_path(r'^snippets/$', views.snippet_list),
-    re_path(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    re_path(r'^snippets/$', views.SnippetList.as_view()),
+    re_path(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
