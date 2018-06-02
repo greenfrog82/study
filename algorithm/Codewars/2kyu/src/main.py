11f83d5fc28dc13963b4912eeecad9e84d03884b
greenfrog <greenfrog82@naver.com>
2018-06-02 13:52:55 +0900

    2kyu Regular Expression - Check if divisible by 0b111 (7)

---
+++ algorithm/Codewars/2kyu/src/main.py
@@ -0,0 +1,20 @@
+from random import random
+
+import unittest
+import re
+
+solution = ''
+
+class Test(unittest.TestCase):
+    
+    def test_case_1(self):
+        self.assertEqual(isinstance(solution,str), True, 'your solution is not a string')
+
+    def test_case_2(self):
+        rgx = re.compile(solution)
+        for num in range(0,101):
+            print('Testing for: '+str(num))
+            self.assertEqual(bool(rgx.match(bin(num)[2:])), num%7 == 0)
+    
+if __name__ == '__main__':
+    unittest.main()
