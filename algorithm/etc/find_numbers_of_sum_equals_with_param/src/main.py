94136f25ee56b29a35e05c43e51f8acbcafabd4d
greenfrog <greenfrog82@naver.com>
2018-06-09 12:40:21 +0900

    Finding numbers of sum equals with param.

--- algorithm/etc/find_numbers_of_sum_equals_with_param/src/main.py
+++ algorithm/etc/find_numbers_of_sum_equals_with_param/src/main.py
@@ -2,15 +2,16 @@ import unittest
 
 def method_1(inputs, n):
     # Method 1. O(n^2)
-    return [(inputs[i], inputs[j]) for i in range(0, len(inputs)) for j in range(i+1, len(inputs)) if n == inputs[i] + inputs[j]]
+    # return [(inputs[i], inputs[j]) for i in range(0, len(inputs)) for j in range(i +1, len(inputs)) if n == inputs[i] + inputs[j]]
+    return [(i_val, j_val) for i, i_val in enumerate(inputs) for _, j_val in enumerate(inputs, i+1) if n == i_val + j_val]
     
 def method_2(inputs, n):
-    # Method 2. O(n)
+    # Method 2. O(n)    
     # return [(inputs[i], n - inputs[i]) for i in range(0, len(inputs)) if n - inputs[i] <= inputs[len(inputs) - 1]][:int(len(inputs)/2)]
-    lst = [(inputs[i], n - inputs[i]) for i in range(0, len(inputs)) if 0 < n - inputs[i] <= inputs[len(inputs) - 1]]
+    lst = [(element, n - element) for element in inputs if n - element in inputs]
     return lst[:int(len(lst)/2)]
 
-find_element = method_2
+find_element = method_1
 
 class Test(unittest.TestCase):
     @classmethod
@@ -36,6 +37,29 @@ class Test(unittest.TestCase):
     def test_5(self):
         ret = find_element(self.inputs, 2)
         self.assertEqual(ret, [])
-        
+
+    def test_6(self):
+        ret = find_element([1, 2, 4, 10], 6)
+        self.assertEqual(ret, [(2, 4)])
+
+    def test_7(self):
+        ret = find_element([1, 2, 4, 10, 5], 6)
+        self.assertEqual(ret, [(1, 5), (2, 4)])
+
+    def test_8(self):
+        ret = find_element([3, 4, 2, 10, 5, 1], 6)
+        self.assertEqual(ret, [(4, 2), (5, 1)])
+
+    def test_9(self):
+        ret = find_element(self.inputs, 4)
+        self.assertEqual(ret, [(1, 3)])
+
+    def test_10(self):
+        ret = find_element(self.inputs, 2)
+        self.assertEqual(ret, [])
+
 if __name__ == '__main__':   
     unittest.main()
+
+# for i,v in enumerate([1, 2, 3, 4], 1):
+#     print('[%d] = %d' % (i, v))
