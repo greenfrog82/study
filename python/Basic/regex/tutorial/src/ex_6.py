97a6a9360aa0a39c27a6d653c732b79b449fe9cc
greenfrog <greenfrog82@naver.com>
2018-06-20 16:42:31 +0900

    About regular expression with python

--- python/Basic/regex/tutorial/src/ex_6.py
+++ python/Basic/regex/tutorial/src/ex_6.py
@@ -10,3 +10,8 @@ print(r"re.match(r'<.*>', '<H1>HEADER</H1>') : ", matched_str)
 matched_str = re.match(r'<.*?>', heading).group()
 print(r"re.match(r'<.*?>', '<H1>HEADER</H1>') : ", matched_str)
 
+
+str = r'tesTtesT'
+
+matched_str = re.match(r'.*?T', str).group()
+print(r"re.match(r'.*T', 'tesTtestT') : ", matched_str)
