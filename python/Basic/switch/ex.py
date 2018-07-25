1a2d2f17fc8c77a62842f58faba11edbe7991c9f
greenfrog <greenfrog82@naver.com>
2018-07-25 13:35:52 +0900

    How to use switch-case in Python

---
+++ python/Basic/switch/ex.py
@@ -0,0 +1,26 @@
+def calc_if(operator, x, y):
+    if operator == 'add':
+        return x + y
+    elif operator == 'sub':
+        return x - y
+    elif operator == 'mul':
+        return x * y
+    elif operator == 'div':
+        return x / y
+    else:
+        return None
+
+
+def calc_dict(operator, x, y):
+    return {
+        'add': lambda: x + y,
+        'sub': lambda: x - y,
+        'mul': lambda: x * y,
+        'div': lambda: x / y,
+    }.get(operator, lambda: None)()
+
+
+print(calc_if('mul', 2, 8))
+print(calc_dict('mul', 2, 8))
+print(calc_if('unknown', 2, 8))
+print(calc_dict('unknown', 2, 8))
