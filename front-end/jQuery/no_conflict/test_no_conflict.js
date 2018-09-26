264c0b26c15678d1d508799d1167485cd1d3de12
greenfrog <greenfrog82@naver.com>
2018-09-26 23:20:47 +0900

    About noConflict function of jQuery

--- front-end/jQuery/no_conflict/test_no_conflict.js
+++ front-end/jQuery/no_conflict/test_no_conflict.js
@@ -3,25 +3,21 @@ e = {}
 e.$ = 100;
 e.jQuery = 500;
 
-var Jt = e.jQuery
+function perform() {
+    var Jt = e.jQuery
     , Kt = e.$;
 
-e.jQuery = 'jQuery';
-e.$ = 'jQuery';
-
-
-w = 'jQuery'
-function perform() {
-    
+    var w = 'jQuery'
+    e.jQuery = e.$ = w
     return w.noConflict = function(t) {
         return e.$ === w && (e.$ = Kt),
         t && e.jQuery === w && (e.jQuery = Jt),
         w
     }
-    // ,
-    // t || (e.jQuery = e.$ = w),
-    // w
 }
 
-console.log(perform()(true))
+// console.log(perform());
+console.log(perform()())
 console.log(e)
+// console.log(perform()(true))
+// console.log(e)
