82eb12350919ffdf22d583a5b911f0f31d74382b
greenfrog82 <greenfrog82@naver.com>
2018-09-28 09:09:38 +0900

    Django-DB DateTimeField and DateField

--- python/Django/django_db/mysite/app_model/models.py
+++ python/Django/django_db/mysite/app_model/models.py
@@ -3,5 +3,6 @@ from django.utils import timezone
 
 class Article(models.Model):
     contents = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
+    # created_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(auto_now=True)
