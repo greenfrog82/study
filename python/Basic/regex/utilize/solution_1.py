1b10373bcc61c8c9fba14f899c6f4edd3c2243d7
greenfrog82 <greenfrog82@naver.com>
2018-10-18 11:20:11 +0900

    Python Regex Utilize - How to remove brace and make new string

--- python/Basic/regex/utilize/solution_1.py
+++ python/Basic/regex/utilize/solution_1.py
@@ -4,13 +4,16 @@ import unittest
 
 def method_me(str_):
     res = re.search(r'^(.+?)\((.+)\)', str_)
+    if not res:
+        return str_
+
     groups = res.groups()
     return u'{} {}'.format(groups[0].strip(), groups[1].strip())
 
 def method_falsetru(str_):
     return re.sub(r'\s*\((.+?)\)', lambda m: ' ' + m.group(1).strip(), str_)
 
-solution = method_falsetru
+solution = method_me
 
 
 class TestSolution(unittest.TestCase):
@@ -19,5 +22,6 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(solution(u'BHC 치킨(강남논현 1호점)'), u'BHC 치킨 강남논현 1호점')
         self.assertEqual(solution(u'BHC 치킨    (강남논현 1호점     )'), u'BHC 치킨 강남논현 1호점')
         self.assertEqual(solution(u'BHC치킨((강남논현점))'), u'BHC치킨 (강남논현점)')
+        self.assertEqual(solution(u'BHC치킨 강남논현점'), u'BHC치킨 강남논현점')
 
 unittest.main()
