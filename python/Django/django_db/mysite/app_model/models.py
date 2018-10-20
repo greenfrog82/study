46eaed8f546c3ee6a01a5dbc21e0263ebfe59ee1
greenfrog <greenfrog82@naver.com>
2018-10-20 23:29:08 +0900

    Python Django - check how to work database migration in test

--- python/Django/django_db/mysite/app_model/models.py
+++ python/Django/django_db/mysite/app_model/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 class Article(models.Model):
@@ -10,7 +11,15 @@ class Article(models.Model):
     #     import pdb; pdb.set_trace()
     #     super(Point, self).save(**kwargs)
 
-
 class Point(models.Model):
     x = models.PositiveIntegerField()
     y = models.PositiveIntegerField()
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    hobby = models.CharField(max_length=16)
+
+    class Meta():
+        ordering = ['-id']
